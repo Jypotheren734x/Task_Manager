10c713881e5d8d8c0bfc90e77ec26e8f4436c27c
Nick <komarnickolas@gmail.com>
2017-07-18 13:02:55 -0600

    Initial Commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,4 +26,5 @@ $(document).ready(function () {
         }
     );
     $('.collapsible').collapsible();
+    $(".button-collapse").sideNav();
 });
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
       sign_up: 'sign_up'
   }
   resources :users, only: [:index, :show]
+  resources :tasks
+  resources :projects
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
