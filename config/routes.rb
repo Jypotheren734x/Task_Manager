c8b7d6c6fe025c50710e00881705cb1ee3c7a846
Nick <komarnickolas@gmail.com>
2017-07-18 23:17:44 -0600

    Reorginized

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :user_groups, only: [:create, :update, :destroy]
+
   devise_for :users, controllers: {
       sessions: 'users/sessions',
       confirmation: 'users/confirmations',
