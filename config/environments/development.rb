441c211229dec749d133dc4327fe9750473c08b5
Nick <komarnickolas@gmail.com>
2017-07-20 19:08:05 -0600

    U

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
